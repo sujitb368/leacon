230ce3014fda91a57f42b2e4e96154db1adb7f77
sujit <sujitb368@gmail.com>
2021-09-28 20:25:59 +0530

    calculation update

--- js/cal.js
+++ js/cal.js
@@ -7,65 +7,102 @@ const donation = document.getElementById("donation");
 const stream = document.getElementById("stream");
 const total_estimate = document.getElementById("total_estimate");
 
-let people = 1;
-let minute ;
-let range ;
+const  plan_town     = document.querySelector(".plan_town")
+const  plan_city   = document.querySelector(".plan_city")
+const  plan_custom   = document.querySelector(".plan_custom")
+const  contact_container   = document.querySelector(".contact_container")
+const  contact   = document.querySelector(".contact")
+
+
+
+let state = {
+     people :  350,
+     minute :  50 ,
+     range  :  50
+} 
+range_text.innerHTML = range_input.value;
 let streamCost = (people, minute)=>{
-    return (people * minute)*0.005;
+    if(people < 500){
+        return (people * minute)*0.007;
+    }else{
+        if(people >= 500 ){
+            return (people * minute)*0.005;
+        }
+    }
+    
 }
 let donationCost = (people , range) =>{
     return (people * range);
 }
 
-
 let totalCost = (x,y)=>{
     return x - y;
 }
 
-
-people_input.addEventListener("input", ()=>{
-    return Number(people = people_input.value);
-
-})
-
-minute_input.addEventListener("input", ()=>{
-   
-    return Number( minute = minute_input.value);
-})
-range_input.addEventListener("input", ()=>{
-    
-    range_text.innerHTML = range_input.value;
-    return Number(range =  range_input.value);
-})
 const cal_op = ()=>{
-    let sc = streamCost(people,minute);
+    let sc = streamCost(state.people , state.minute);
     stream.innerHTML = sc;
    
-    let dc = donationCost(people,range);
+    let dc = donationCost(state.people , state.range);
     donation.innerHTML = dc
 
     let tc = totalCost(dc, sc)
     total_estimate.innerHTML = tc;
     
 }
+const change_cart = (people_input)=>{
+    if(people_input >= 500 && people_input <= 1000){
 
-people_input.addEventListener("input", function(){
+        plan_town.classList.add("hide");
+        plan_city.classList.remove("hide");
+        plan_custom.classList.add("hide")
+        contact.classList.add("hide")
+        contact_container.classList.add("hide")
+    }else if(people_input > 1000){
+        plan_town.classList.add("hide");
+        plan_city.classList.add("hide");
+        plan_custom.classList.remove("hide")
+        contact.classList.remove("hide")
+        contact_container.classList.remove("hide")
     
-    if(range != undefined){
-        cal_op();
+    }else{
+        plan_town.classList.remove("hide");
+        contact.classList.add("hide")
+        contact_container.classList.add("hide")
+        plan_city.classList.add("hide");
+        plan_custom.classList.add("hide")
     }
-    
-    
-})
-minute_input.addEventListener("input", function(){
-    
-    if(range != undefined){
-        cal_op();
+}
+cal_op();
+people_input.addEventListener("input", ()=>{
+    if(people_input.value != ""){
+        parseInt(state.people = people_input.value);
+        console.log(state.people);
+        console.log(typeof(state.people))
+        cal_op()
+        change_cart(people_input.value)
+
     }
 })
-range_input.addEventListener("input", function(){
-    
-    if(range != undefined){
-        cal_op();
+
+minute_input.addEventListener("input", ()=>{
+    if(minute_input.value != ""){
+        parseInt(state.minute = minute_input.value);
+        cal_op()
+
     }
 })
+range_input.addEventListener("input", ()=>{    
+    if(range_input.value != ""){
+        // range_input.style("range_bar::-webkit-slider-thumb", `left:${range_input.value}%`)
+        parseInt(state.range = range_input.value);
+
+        range_text.innerHTML = range_input.value;
+    
+        cal_op()
+
+    }
+})
+
+
+
